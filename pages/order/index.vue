e646e520875fc04d843f9f5e614810c641eaac89
prasetyo <tryprasetyoadi2420@gmail.com>
2024-06-04 01:41:34 +0700

    push

---
+++ pages/order/index.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Layout Default</h3>
+                    <p class="text-subtitle text-muted">The default layout </p>
+                </div>
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><nuxt-link to="/">Dashboard</nuxt-link></li>
+                            <li class="breadcrumb-item active" aria-current="page">Layout Default</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <section class="section">
+            <div class="row">
+                <div class="card col-md-3" style="margin-right: 80px; margin-left: 50px;">
+                    <div class="card-content">
+                        <img src="~assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid"
+                            alt="singleminded" >
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="buttons" style="margin-left: auto; margin-right: auto">
+                            <a href="#" class="btn btn-outline-primary">Primary</a>
+                    </div>
+                </div>
+                <div class="card col-md-3" style="margin-right: 80px;">
+                    <div class="card-content">
+                        <img src="~assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid"
+                            alt="singleminded" >
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="buttons" style="margin-left: auto; margin-right: auto">
+                            <a href="#" class="btn btn-outline-primary">Primary</a>
+                    </div>
+                </div>
+                <div class="card col-md-3" style="margin-right: 80px;">
+                    <div class="card-content">
+                        <img src="~assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid"
+                            alt="singleminded" >
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="buttons" style="margin-left: auto; margin-right: auto">
+                            <a href="#" class="btn btn-outline-primary">Primary</a>
+                    </div>
+                </div>
+                <div class="card col-md-3" style="margin-right: 80px; margin-left: 50px">
+                    <div class="card-content">
+                        <img src="~assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid"
+                            alt="singleminded" >
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="buttons" style="margin-left: auto; margin-right: auto">
+                            <a href="#" class="btn btn-outline-primary">Primary</a>
+                    </div>
+                </div>
+                <div class="card col-md-3" style="margin-right: 80px;">
+                    <div class="card-content">
+                        <img src="~assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid"
+                            alt="singleminded" >
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="buttons" style="margin-left: auto; margin-right: auto">
+                            <a href="#" class="btn btn-outline-primary">Primary</a>
+                    </div>
+                </div>
+                <div class="card col-md-3" style="margin-right: 80px;">
+                    <div class="card-content">
+                        <img src="~assets/images/samples/motorcycle.jpg" class="card-img-top img-fluid"
+                            alt="singleminded" >
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="buttons" style="margin-left: auto; margin-right: auto">
+                            <a href="#" class="btn btn-outline-primary">Primary</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    </template>
+    
